636c101d8dc69cfa2aa6dcbfacdabe82726d11b5
André Marquetto <andre-marquettoo@hotmail.com>
2026-04-16 22:03:40 -0300

    SUBINDO FUNCIONANDO COM BD

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from db import execute_query
 
 app = Flask(__name__)
 app.secret_key = 'imc_secret_key_2026'
@@ -6,6 +7,10 @@ app.secret_key = 'imc_secret_key_2026'
 
 @app.route('/')
 def index():
+    sql = "SHOW TABLES;"
+    resultado = execute_query(sql, fetch=True)
+    print(resultado)
+
     return render_template('index.html')
 
 
@@ -17,6 +22,30 @@ def resultados():
 
 @app.route('/calcular', methods=['GET', 'POST'])
 def calcular():
+
+    if request.method == 'POST':
+        nome = request.form.get('nome', 'Não foi enviado um nome!')
+        peso = request.form.get('peso', 'Não foi informado nenhum peso')
+        altura = request.form.get('altura', 'Não foi informada altura!')
+
+        peso = float(peso)
+        altura = float(altura)
+
+        imc = round (peso / (altura ** 2), 2) 
+
+        if imc < 18.5:
+            classificacao = 'Abaixo do peso'
+        elif imc < 25:
+            classificacao = 'Peso normal'
+        ## Adicionar as demais classificações
+        
+        else:
+            classificacao = 'Erro no cálculo do IMC'
+
+        flash(f'Olá {nome}, seu IMC é: {imc} - Classificacao:{classificacao}', 'success')
+
+        return redirect(url_for('resultados'))
+    
     return render_template('formulario.html')
 
 

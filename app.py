c51f2be4a8586bbf525ae05c7a89556cf4e36cc3
André Marquetto <andre-marquettoo@hotmail.com>
2026-04-30 19:51:47 -0300

    novo codigo'

--- app.py
+++ app.py
@@ -4,48 +4,100 @@ from db import execute_query
 app = Flask(__name__)
 app.secret_key = 'imc_secret_key_2026'
 
+def calcular_imc(peso, altura ):
+    return round(peso / (altura ** 2), 2) 
+
+def classificacao(imc):
+    if imc < 18.5:
+        classificacao = 'Abaixo do peso'
+    elif imc < 25:
+        classificacao = 'Peso normal'
+    elif imc < 30:
+        classificacao = 'Sobrepeso'
+
+    # Demais classificações
+    else:
+        classificacao = 'Erro na classificação'    
+    
+    return classificacao
+
 
 @app.route('/')
 def index():
-    sql= "SHOW TABLES;"
-    resultados= execute_query(sql, fetch=True)
-    print(resultados)
+    sql = '''
+CREATE TABLE IF NOT EXISTS calculos (
+    id_calculo BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    peso DECIMAL(6, 2) NOT NULL,
+    altura DECIMAL(5, 2) NOT NULL,
+
+    criado_em DATETIME DEFAULT CURRENT_TIMESTAMP,
+    alterado_em DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deletado_em DATETIME NULL
+);
+'''
+    resultado = execute_query(sql, fetch=True)
+    print(resultado)
 
     return render_template('index.html')
 
 
 @app.route('/resultados')
 def resultados():
-    calculos = []
-    return render_template('resultados.html', calculos=calculos, total=len(calculos))
+
+    try:
+
+        sql = "SELECT * FROM calculos WHERE deletado_em IS NULL"
+
+        calculos = execute_query(sql, fetch=True)
+
+        if calculos is None:
+            calculos = []
+
+    except Exception as e:
+        flash (f'Erro ao buscar dados!', 'danger')
+        app.logger.error(f'Erro no INSERT: {E}')
+        return redirect (url_for('resultados'))
+
+    return render_template('resultados.html', 
+                           calculos=calculos, 
+                           total=len(calculos),
+                           calcular = calcular_imc,
+                           classificacao = classificacao)
 
 
 @app.route('/calcular', methods=['GET', 'POST'])
 def calcular():
 
     if request.method == 'POST':
-        nome = request.form.get('nome', 'Não foi enviado um nome!')
-        peso = request.form.get('peso', 'Não foi informado nenhum peso')
-        altura = request.form.get('altura', 'Não foi informada altura!')
+        nome = request.form.get('nome', 'Não foi enviado nome!').strip()
+        peso = request.form.get('peso', 'Não foi enviado peso!').strip()
+        altura = request.form.get('altura', 'Não foi enviado altura!').strip()
 
         peso = float(peso)
         altura = float(altura)
 
-        imc = round (peso / (altura ** 2), 2) 
+        try:
+            # Cria o SCRIPT SQL para ser enviado, %s é cada valor 
+            sql = 'INSERT INTO calculos(nome, peso, altura) VALUES (%s, %s, %s);'
 
-        if imc < 18.5:
-            classificacao = 'Abaixo do peso'
-        elif imc < 25:
-            classificacao = 'Peso normal'
-        ## Adicionar as demais classificações
+            # Passa o SQL + os parametros que aqui são os dados em uma lista 
+            execute_query(sql, (nome, peso, altura))
+
+            # Gera a notificação de sucesso
+            flash(f'Produto [{nome}] cadastrado com sucesso!', 'sucesso')    
+           
+            # Leva a tela de resultados 
+            return redirect (url_for('resultados'))
+
+
+        except Exception as e:
+            flash (f'Erro ao salvar!', 'danger')
+            app.logger.error(f'Erro no INSERT: {E}')
+            return redirect (url_for('calcular'))
         
-        else:
-            classificacao = 'Erro no cálculo do IMC'
+        # flash(f'Olá {nome}, seu IMC é {imc} e sua classificação é: {classificacao}', 'success')    
 
-        flash(f'Olá {nome}, seu IMC é: {imc} - Classificacao:{classificacao}', 'success')
-
-        return redirect(url_for('resultados'))
-    
     return render_template('formulario.html')
 
 
--- db.py
+++ db.py
@@ -7,23 +7,24 @@ DB_PARAMS = {
     'password': '',
     'database': 'imc_python',
     'charset': 'utf8mb4',
+    'time_zone': '-03:00',
     'use_pure': True,
     'connect_timeout': 10
 }
 
 _pool = pooling.MySQLConnectionPool(
     pool_name='imc_pool',
-    pool_size=5,
+    pool_size=5, 
     **DB_PARAMS
-)
+    )
 
 def get_connection():
     try:
         return _pool.get_connection()
-    except Error as e:
-        raise Exception(f'Erro ao obter conexão do pool: {e}')
-    
 
+    except Error as e:
+        raise Exception(f"Erro ao obter conexão do pool: {e}")
+    
 def execute_query(sql, params=None, fetch=False):
     conn = get_connection()
     try:
@@ -35,10 +36,14 @@ def execute_query(sql, params=None, fetch=False):
         else:
             conn.commit()
             return cursor.rowcount
-
+        
     except Error as e:
         conn.rollback()
-        raise Exception(f'Erro ao executar a query: {e}')
+        raise Exception(f"Erro ao executar query: {e}")
     finally:
         cursor.close()
         conn.close()
+
+def execute_one(sql, params=None):
+    resultados =  execute_query(sql, params, fetch=True)
+    return resultados[0] if resultados else None
